7d0d0fa4496f8f850669d39804fbb4a677573bad
fernanda <fernandatempestab@gmail.com>
2017-06-29 17:41:34 -0300

    versãod a entrevista

--- main.cpp
+++ main.cpp
@@ -21,8 +21,45 @@ void imprime_matriz(int linha, int coluna, int **matriz){
 	cout << endl;
 }
 
-int busca_caminho(int qnt_vertice, int *caminho, int *visitado, queue<int> &fila,
-    int s, int t, int **residual){
+void marca_caminho(int qnt_vertice, int qnt_aresta, int *caminho_aresta,
+    int *caminho, int **N, int *novo_custo, int **residual){
+    int vertice_anterior = -1;
+    for(int k = qnt_vertice - 1; k >= 0; k--){
+        vertice_anterior = caminho[k];
+        if(vertice_anterior != -1){
+            for(int j = 0; j < qnt_aresta; j++){
+                if((N[k][j] == 1) && (N[vertice_anterior][j] == -1)){
+                    caminho_aresta[j] = 1;
+                }
+            }
+        }
+    }
+    cout<<"[";
+    for(int j = 0; j < qnt_aresta; j++){
+        cout<< caminho_aresta[j] << " ";
+    }
+    cout<<"]"<<endl;
+    for(int i = 0; i < qnt_vertice-1; i++){
+        for(int j = 0; j < qnt_aresta; j++){
+            if(N[i][j] == -1){
+                for(int k = 0; k < qnt_vertice; k++){
+                    if(N[k][j] == 1){
+                        if(caminho_aresta[j] == 1){
+                            novo_custo[j] = residual[i][k];
+                        }
+                    }
+                }
+            }
+        }
+	}
+	cout<<"[";
+	for(int j = 0; j < qnt_aresta; j++){
+        cout<< novo_custo[j] << " ";
+    }
+}
+
+int busca_caminho(int qnt_vertice, int qnt_aresta, int *caminho, int *visitado, queue<int> &fila,
+    int s, int t, int **residual, int *caminho_aresta, int **N, int *novo_custo, int *custo){
 
     bool existe_caminho = false;
 
@@ -47,22 +84,28 @@ int busca_caminho(int qnt_vertice, int *caminho, int *visitado, queue<int> &fila
     }
     if(visitado[t])
         existe_caminho = true;
+    marca_caminho(qnt_vertice, qnt_aresta, caminho_aresta, caminho, N, novo_custo, residual);
+    cout<<"]"<<endl;
+    cout<<"[";
+	for(int j = 0; j < qnt_aresta; j++){
+        cout<< custo[j] << " ";
+    }
+    cout<<"]"<<endl;
     return existe_caminho;
 }
 
-int retorna_fluxo_max(int qnt_vertice, int **residual, int *caminho,
-    int t, int s, int *visitado, queue<int> &fila){
+int retorna_fluxo_max(int qnt_vertice, int qnt_aresta, int **residual, int *caminho,
+    int t, int s, int *visitado, queue<int> &fila, int *caminho_aresta, int **N, int *novo_custo, int *custo){
+
     int fluxo_max = 0;
 
-
-    while(busca_caminho(qnt_vertice, caminho, visitado, fila, s, t, residual)){
+    while(busca_caminho(qnt_vertice, qnt_aresta, caminho, visitado, fila, s, t, residual, caminho_aresta, N, novo_custo, custo)){
         int fluxo = 999999999;
         int v = 0;
 
         for(int i = t; i != s; i = caminho[i]){
             v = caminho[i];
             if(residual[v][i] < fluxo){
-                //fluxo = min(fluxo, residual[v][i])
                 fluxo = residual[v][i];
             }
         }
@@ -72,15 +115,10 @@ int retorna_fluxo_max(int qnt_vertice, int **residual, int *caminho,
             residual[v][i] -= fluxo;
             residual[i][v] += fluxo;
         }
-        for(int i = 0; i < qnt_vertice; i++){
-            for(int j = 0; j< qnt_vertice; j++){
-                cout<<residual[i][j] << " ";
-            }
-            cout <<endl;
-        }
-        cout<< endl;
         fluxo_max += fluxo;
+        cout<<endl;
     }
+
     return fluxo_max;
 }
 
@@ -95,26 +133,22 @@ int main() {
     //lê dados de entrada
     ifstream file("input.txt");
     file >> qnt_vertice;
-    cout << qnt_vertice << endl;
     file >> qnt_aresta;
-    cout << qnt_aresta<< endl;
     int *custo = new int[qnt_aresta];
     for(int j = 0; j < qnt_aresta; j++){
         file >> custo[j];
     }
     //imprime_vetor(qnt_aresta, custo);
-    cout <<endl;
     //cria a matriz de incidência
     int **N = new int*[qnt_vertice];    // cria primeiro as linhas
     for(int i = 0; i < qnt_vertice; i++)
         N[i] = new int[qnt_aresta];      //cria as colunas
 
-    //Recebe matriz de adjacência do arquivo
+    //Recebe matriz de incidência do arquivo
 	for (int i = 0; i < qnt_vertice; i++){
 		for (int j = 0; j < qnt_aresta; j++)
 			file >> N[i][j];
 	}
-	cout << endl;
 	//imprime_matriz(qnt_vertice, qnt_aresta, N);
 
 	int **adjacencia = new int*[qnt_vertice];
@@ -133,7 +167,6 @@ int main() {
             }
         }
 	}
-	cout << endl;
 	//imprime_matriz(qnt_vertice, qnt_vertice, adjacencia);
 
     int s = 0;              //origem
@@ -149,10 +182,25 @@ int main() {
         for(int j = 0; j < qnt_vertice; j++)
             residual[i][j] = adjacencia[i][j];
     }
-    cout << endl;
-    //imprime_matriz(qnt_vertice, qnt_vertice, residual);
-    int fluxo_maximo = retorna_fluxo_max(qnt_vertice, residual, caminho, t, s, visitado, fila);
+
+    int *caminho_aresta = new int[qnt_aresta]();
+    int *novo_custo = new int[qnt_aresta]();
+    int fluxo_maximo = retorna_fluxo_max(qnt_vertice, qnt_aresta, residual,
+    caminho, t, s, visitado, fila, caminho_aresta, N, novo_custo, custo);
+    cout<<endl;
     cout << "fluxo maximo: "<< fluxo_maximo <<endl;
+    int *corte_minimo = new int[qnt_aresta]();
+    for(int i= 0; i < qnt_aresta; i++){
+        if(custo[i] == novo_custo[i]){
+            corte_minimo[i] = 1;
+        }
+    }
+    cout<<endl;
+    cout<<"[";
+    for(int j = 0; j < qnt_aresta; j++){
+        cout<< corte_minimo[j] << " ";
+    }
+    cout<<"]"<<endl;
 
     return 0;
 }
